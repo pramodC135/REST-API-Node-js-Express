83d9f546292bc674e70a465c552dc0aef88676b9
pramodC135 <pramodmadhushan4@gmail.com>
2021-05-18 10:58:43 +0530

    multi para ports

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.get('/api/courses', (req, res) => {
     res.send([1,2,3]);
 });
 
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params);
+});
+
 //PORT
 const port = process.env.PORT || 3000;
 app.listen(port,() => console.log(`Listening on port ${port}...`));
