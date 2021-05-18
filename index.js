204809ae71f647e72cada57ab7408266982ac00b
pramodC135 <pramodmadhushan4@gmail.com>
2021-05-18 10:50:02 +0530

    port change

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ app.get('/', (req, res) => {
 app.get('/api/courses', (req, res) => {
     res.send([1,2,3]);
 });
-app.listen(3000,() => console.log('Listening on port 3000...'));
+
+//PORT
+const port = process.env.PORT || 3000;
+app.listen(port,() => console.log(`Listening on port ${port}...`));
