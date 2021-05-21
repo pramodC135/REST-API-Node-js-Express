66de7eebc9fc9798f6dddf52408388816c0b0d33
pramodC135 <pramodmadhushan4@gmail.com>
2021-05-21 10:45:33 +0530

    post method

--- index.js
+++ index.js
@@ -24,7 +24,12 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) =>{
-
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name;
+    };
+    courses.push(course);
+    res.send(course);
 });
 
 app.get('/api/posts/:year/:month', (req, res) => {
