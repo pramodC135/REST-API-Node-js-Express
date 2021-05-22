b2661f1ca86353778e4d512a253234677233f72f
pramodC135 <pramodmadhushan4@gmail.com>
2021-05-22 23:03:42 +0530

    upd

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/api/courses', (req, res) =>{
 
     const result = Joi.validate(req.body, schema);
     console.log(result);
-
+ 
     if (!req.body.name || req.body.length < 3) {
         // 400 Bad Request
         res.status(400).send('Name is required and should be minimum 3 characters.');
