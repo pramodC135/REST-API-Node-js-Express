01cdd8b0f377b1dc25fb7d3fd8cf51f45e058dea
pramodC135 <pramodmadhushan4@gmail.com>
2021-05-24 23:34:14 +0530

    add hello world

--- apptest.js
+++ apptest.js
@@ -3,7 +3,7 @@ const http =  require('http');
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
-        
+        res.write('Hello world');
         res.end();
     }
 
